f1ed6c02525b6dc88f71c25c7e20871ab496cb33
ChrisMicro <christoph@roboterclub-freiburg.de>
2017-05-19 20:08:59 +0200

    initial commit

---
+++ _010_test/_010_test.ino
@@ -0,0 +1,25 @@
+#define POTI_BACKWARD_MOTOR1
+#define POTI_VORWARD_MOTOR1
+#define POTI_CYCLETIME_MOTOR1
+#define VOLTAGE_MOTOR1
+
+void setup() 
+{
+
+}
+
+unsigned long Time_ms  = 0;
+int           Timer_ms = 0;
+
+void loop() 
+{
+  
+
+  
+  while(Time_ms==millis(); // wait for next millisecond
+  Time_ms=millis();
+  
+  Timer_ms++;
+  
+
+}
