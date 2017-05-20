3242a3846b61317b622a5024da1a764d84e0203c
elektrouwe <uwe.schueler@uni-tuebingen.de>
2017-05-20 11:49:53 +0200

    test corrected

--- _010_test/_010_test.ino
+++ _010_test/_010_test.ino
@@ -1,79 +1,44 @@
-//#define BLUEPILL
+/*
+  hardware test for the
+
+  Klopfer Percusive Controller
+
+  May 2017, ChrisMicro
+*/
 
 
-#ifdef BLUEPILL
+#include "klopfer_pin_definitions.h"
 
 #define POTI_BACKWARD_MOTOR1   PA5
 #define POTI_VORWARD_MOTOR1    PA6
 #define POTI_CYCLETIME_MOTOR1  PA7
 #define VOLTAGE_MOTOR1         PA17
 
-#define LED0 PB6
-#define LED1 PB7
-#define LED2 PB8
-#define LED3 PB9
-
-#else
-
-#define POTI_BACKWARD_MOTOR1   A0
-#define POTI_VORWARD_MOTOR1    A1
-#define POTI_CYCLETIME_MOTOR1  A2
-#define VOLTAGE_MOTOR1         12
-
-#define LED0 3
-#define LED1 4
-#define LED2 5
-#define LED3 6
-
-#endif
 
 void setup()
 {
-#ifdef BLUEPILL
-  pinMode(POTI_CYCLETIME_MOTOR1, INPUT_ANALOG);
-  pinMode(VOLTAGE_MOTOR1, PWM);
-#else
-  pinMode(POTI_CYCLETIME_MOTOR1, INPUT);
-  pinMode(VOLTAGE_MOTOR1, OUTPUT);
-#endif
-
-  pinMode(LED0, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
 
-  digitalWrite(LED0, 1);
-  digitalWrite(LED1, 1);
-  digitalWrite(LED2, 1);
-  digitalWrite(LED2, 1);
+  pinMode(TRIG1, INPUT_ANALOG);
+  pinMode(TRIG2, INPUT_ANALOG);
+  pinMode(TRIG3, INPUT_ANALOG);
+  pinMode(TRIG4, INPUT_ANALOG);
 
+  pinMode(POTI1, INPUT_ANALOG);
+  pinMode(POTI2, INPUT_ANALOG);
+  pinMode(POTI3, INPUT_ANALOG);
+  pinMode(POTI4, INPUT_ANALOG);
+  pinMode(POTI5, INPUT_ANALOG);
+  pinMode(POTI6, INPUT_ANALOG);
 }
 
-unsigned long Time_ms  = 0;
-int           Timer1_ms = 0;
 
 void loop()
 {
-  int timerLimit = analogRead(POTI_CYCLETIME_MOTOR1);
-
-  int zero_voltage_motor1     = 128;
-  int vorward_voltage_motor1  = 200;
-  int backward_voltage_motor1 = 20;
-
-  if ( Timer1_ms >= timerLimit )
-  {
-    digitalWrite(LED0,1);
-    analogWrite(VOLTAGE_MOTOR1, vorward_voltage_motor1);
-    delay(10);
-    digitalWrite(LED0,0);
-    analogWrite(VOLTAGE_MOTOR1, backward_voltage_motor1);
-    Timer1_ms = 0;
-  }
-
-  while (Time_ms == millis()); // wait for next millisecond
-  Time_ms = millis();
-
-  Timer1_ms++;
-
+    digitalWrite(LED1, !digitalRead(LED1)); // toggle led
+    delay(100);
 
 }
---
+++ _010_test/klopfer_pin_definitions.h
@@ -0,0 +1,22 @@
+
+// LEDS
+#define LED1 PB12
+#define LED2 PB13
+#define LED3 PB11
+#define LED4 PB10
+
+// VC trigger inputs
+#define TRIG1 PA0
+#define TRIG2 PA1
+#define TRIG3 PA2
+#define TRIG4 PA3
+
+// POTIS
+#define POTI1 PA4
+#define POTI2 PA5
+#define POTI3 PA6
+#define POTI4 PA7
+#define POTI5 PB0
+#define POTI6 PB1
+
+
